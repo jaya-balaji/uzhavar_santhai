bc5c9905666399b480a3b6ad9692439f52187c4d
jaya-balaji <balabavan1013@gmail.com>
2025-01-29 12:55:22 +0400

    validation

--- client/src/app/adminsignup/page.tsx
+++ client/src/app/adminsignup/page.tsx
@@ -58,7 +58,7 @@ const validatePassword=()=> {
   return (
     <div className="flex items-center justify-center h-[100vh] background">
       <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center gap-4 md:gap-8 bg-black p-10 md:p-16 rounded-3xl bg-opacity-70 md:w-[70vh]">
-        <div className="text-white">
+        <div className="text-white text-xs">
           {message?message:""}
         </div>
         <div className="flex flex-row gap-8 justify-between items-center">
