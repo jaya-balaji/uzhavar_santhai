00c49415e5de2af3d37f006bd9dfa8005228b17b
jaya-balaji <balabavan1013@gmail.com>
2025-01-29 12:10:11 +0400

    interface change

--- client/src/app/adminsignup/page.tsx
+++ client/src/app/adminsignup/page.tsx
@@ -35,7 +35,7 @@ const page = () => {
 
   return (
     <div className="flex items-center justify-center h-[100vh] background">
-      <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center gap-4 md:gap-8 bg-black p-10 md:p-16 rounded-3xl bg-opacity-70 md:w-[70vh]">
+      <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center gap-4 md:gap-8 bg-black p-10 md:p-16 bg-opacity-70 md:w-[70vh]">
         <div className="text-white">
           {message}
         </div>
